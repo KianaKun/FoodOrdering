1bdbc7450c9d15315c18a4f6d31a849fd9ce556d
KianaKun <hilmananz@gmail.com>
2024-05-09 11:23:41 +0700

    Update Mainapps

--- app/src/main/java/com/example/foodordering/mainapps.kt
+++ app/src/main/java/com/example/foodordering/mainapps.kt
@@ -2,6 +2,7 @@ package com.example.foodordering
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class mainapps : AppCompatActivity() {
@@ -17,9 +18,100 @@ class mainapps : AppCompatActivity() {
     private lateinit var buttonMinNasiGoreng: Button
     private lateinit var buttonMinEsTeh: Button
     private lateinit var buttonMinEsJeruk: Button
+    private lateinit var sopIgaValue: TextView
+    private lateinit var nasiUdukValue: TextView
+    private lateinit var mieGorengValue: TextView
+    private lateinit var nasiGorengValue: TextView
+    private lateinit var esTehValue: TextView
+    private lateinit var esJerukValue: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainapps);
+
+        // Value Button
+        buttonAddSopIga=findViewById(R.id.buttonAddSopIga)
+        buttonMinSopIga=findViewById(R.id.buttonMinSopIga)
+        buttonAddNasiUduk=findViewById(R.id.buttonAddNasiUduk)
+        buttonMinNasiUduk=findViewById(R.id.buttonMinNasiUduk)
+        buttonAddMieGoreng=findViewById(R.id.buttonAddMieGoreng)
+        buttonMinMieGoreng=findViewById(R.id.buttonMinMieGoreng)
+        buttonAddNasiGoreng=findViewById(R.id.buttonAddNasiGoreng)
+        buttonMinNasiGoreng=findViewById(R.id.buttonMinNasiGoreng)
+        buttonAddEsTeh=findViewById(R.id.buttonAddEsTeh)
+        buttonMinEsTeh=findViewById(R.id.buttonMinEsTeh)
+        buttonAddEsJeruk=findViewById(R.id.buttonAddEsJeruk)
+        buttonMinEsJeruk=findViewById(R.id.buttonMinEsJeruk)
+
+        //Value TextView
+        sopIgaValue=findViewById(R.id.sopIgaValue)
+        nasiUdukValue=findViewById(R.id.nasiUdukValue)
+        mieGorengValue=findViewById(R.id.mieGorengValue)
+        nasiGorengValue=findViewById(R.id.nasiGorengValue)
+        esTehValue=findViewById(R.id.esTehValue)
+        esJerukValue=findViewById(R.id.esJerukValue)
+
+        buttonAddSopIga.setOnClickListener{
+            var valueSopIga = sopIgaValue.text.toString().toInt()
+            valueSopIga++
+            sopIgaValue.text = valueSopIga.toString()
+        }
+        buttonMinSopIga.setOnClickListener{
+            var valueSopIga = sopIgaValue.text.toString().toInt()
+            valueSopIga--
+            sopIgaValue.text = valueSopIga.toString()
+        }
+        buttonAddNasiUduk.setOnClickListener{
+            var valueNasiUduk = nasiUdukValue.text.toString().toInt()
+            valueNasiUduk++
+            nasiUdukValue.text = valueNasiUduk.toString()
+        }
+        buttonMinNasiUduk.setOnClickListener{
+            var valueNasiUduk = nasiUdukValue.text.toString().toInt()
+            valueNasiUduk--
+            nasiUdukValue.text = valueNasiUduk.toString()
+        }
+        buttonAddMieGoreng.setOnClickListener{
+            var valueMieGoreng = mieGorengValue.text.toString().toInt()
+            valueMieGoreng++
+            mieGorengValue.text = valueMieGoreng.toString()
+        }
+        buttonMinMieGoreng.setOnClickListener{
+            var valueMieGoreng = mieGorengValue.text.toString().toInt()
+            valueMieGoreng--
+            mieGorengValue.text = valueMieGoreng.toString()
+        }
+        buttonAddNasiGoreng.setOnClickListener{
+            var valueNasiGoreng = nasiGorengValue.text.toString().toInt()
+            valueNasiGoreng++
+            nasiGorengValue.text = valueNasiGoreng.toString()
+        }
+        buttonMinNasiGoreng.setOnClickListener {
+            var valueNasiGoreng = nasiGorengValue.text.toString().toInt()
+            valueNasiGoreng--
+            nasiGorengValue.text = valueNasiGoreng.toString()
+        }
+        buttonAddEsTeh.setOnClickListener {
+            var valueEsTeh = esTehValue.text.toString().toInt()
+            valueEsTeh++
+            esTehValue.text = valueEsTeh.toString()
+        }
+        buttonMinEsTeh.setOnClickListener {
+            var valueEsTeh = esTehValue.text.toString().toInt()
+            valueEsTeh--
+            esTehValue.text = valueEsTeh.toString()
+        }
+        buttonAddEsJeruk.setOnClickListener {
+            var valueEsJeruk = esJerukValue.text.toString().toInt()
+            valueEsJeruk++
+            esJerukValue.text = valueEsJeruk.toString()
+        }
+        buttonMinEsJeruk.setOnClickListener {
+            var valueEsJeruk = esJerukValue.text.toString().toInt()
+            valueEsJeruk--
+            esJerukValue.text = valueEsJeruk.toString()
+        }
     }
 }
