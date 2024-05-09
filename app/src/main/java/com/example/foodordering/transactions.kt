e7ddae8c40dae3e1fc113aaf830fbcafaadddf06
KianaKun <hilmananz@gmail.com>
2024-05-09 15:17:32 +0700

    Setting Margin

--- app/src/main/java/com/example/foodordering/transactions.kt
+++ app/src/main/java/com/example/foodordering/transactions.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class transactions : AppCompatActivity() {
     private lateinit var submittransaction: Button
     private lateinit var canceltransaction: Button
+    private lateinit var view: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +17,7 @@ class transactions : AppCompatActivity() {
 
         submittransaction = findViewById(R.id.buttonSubmit)
         canceltransaction = findViewById(R.id.buttonCancel)
+        view = findViewById(R.id.recyclerView)
 
         canceltransaction.setOnClickListener {
             val intent = Intent(this, mainapps::class.java)
@@ -22,7 +25,7 @@ class transactions : AppCompatActivity() {
         }
 
         submittransaction.setOnClickListener {
-            val intent = Intent(this, mainapps::class.java)
+            val intent = Intent(this, inputuser::class.java)
             startActivity(intent)
         }
     }
