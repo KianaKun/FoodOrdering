33d5fddcb790a346bbbd378a7649b10eee792175
KianaKun <hilmananz@gmail.com>
2024-05-09 12:11:19 +0700

    Update inputuser.kt and inputuser.xml -Nomor Meja + Some Filtering

--- app/src/main/java/com/example/foodordering/inputuser.kt
+++ app/src/main/java/com/example/foodordering/inputuser.kt
@@ -3,15 +3,15 @@ package com.example.foodordering
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.widget.Toast
 
 class inputuser : AppCompatActivity() {
     private lateinit var inputButton: Button
     private lateinit var textPelayan: EditText
     private lateinit var textPelanggan: EditText
-    private lateinit var copyright: TextView
+    private lateinit var nomorMeja: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inputuser)
@@ -19,10 +19,16 @@ class inputuser : AppCompatActivity() {
         inputButton = findViewById(R.id.inputButton)
         textPelayan = findViewById(R.id.TextPelayan)
         textPelanggan = findViewById(R.id.TextPelanggan)
+        nomorMeja= findViewById(R.id.nomorMeja)
 
         inputButton.setOnClickListener {
             val intent = Intent(this, mainapps::class.java)
-            startActivity(intent);
+            if (textPelayan.text.isEmpty() and textPelanggan.text.isEmpty() and nomorMeja.text.isEmpty() ) {
+                Toast.makeText(this, "Nama Pelayan atau Nama Pelanggan Kosong atau Meja Kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(intent)
+            }
+            
         }
     }
 }
